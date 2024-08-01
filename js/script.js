299a2d7a3042aba9a8e1ddea05b59d410b34d5df
ParamitaNurul12 <paramitanw12@gmail.com>
2024-08-01 19:28:52 +0700

    Save point 2
    Added form validation.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 // Ini Javascript
 
 console.log('hello world');
+
+// Ini untuk form validation nama
+
+function formValidation() {
+  let name = document.getElementById('fullname').value;
+  console.log(name);
+
+  //Pengecekan name tidak boleh ' '
+  if (name == '') {
+    // code akan di eksekusi ketika name ' '
+    alert('Name cannot be empty');
+    //code akan di eksekusi ketika name terisi dan di input
+  } else {
+    alert('Submitted succesfully');
+  }
+}
+
+//ini untuk form validation email
+
+function formValidation() {
+  let name = document.getElementById('email').value;
+  console.log(name);
+
+  //Pengecekan email tidak boleh ' '
+  if (name == '') {
+    // code akan di eksekusi ketika email ' '
+    alert('Email cannot be empty');
+    //code akan di eksekusi ketika email terisi dan di input
+  } else {
+    alert('Submitted succesfully');
+  }
+}
